19132e691580249858a44243fd042b5dd4592840
husniddinuzinfocom <105488050+husniddinuzinfocom@users.noreply.github.com>
2022-06-20 14:26:25 +0500

    initial commit

--- src/components/Lists/Lists.js
+++ src/components/Lists/Lists.js
@@ -1,25 +1,81 @@
-import React from 'react';
-import TodoItem from '../TodoItem/TodoItem';
+import React, { useEffect, useState } from "react";
+import TodoItem from "../TodoItem/TodoItem";
 import classes from "./Lists.module.css";
 
-export default function Lists({ todoList }) {
-    const todoItem = todoList.length ? todoList.map((item) => {
-                            return <TodoItem 
-                                        title={item.title} 
-                                        id={item.id} 
-                                        isCompleted={item.isCompleted} 
-                                        key={item.id} />
-                    }) : "";
-    return (
-        <main>
-            <select className={classes.selectFilter}>
-                <option value="all">All</option>
-                <option value="completed">Completed</option>
-                <option value="uncompleted">Uncompleted</option>
-            </select>
-            <section className={classes.task_list}>
-                {todoItem}
-            </section>
-        </main>
-    )
+export default function Lists({
+  updateId,
+  setUpdateId,
+  inpRef,
+  setControllerUpdateToAdd,
+  setCurrentItem,
+}) {
+  const [data, setData] = useState([]);
+  const [filtredData, setFiltredData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [updateId]);
+
+  const getData = async () => {
+    const url = "https://todoapidrf.pythonanywhere.com/api/todos/";
+    const token = JSON.parse(localStorage.getItem("token"));
+
+    const response = fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    });
+    const data = await response.then((res) => res.json());
+    setData(data);
+    setFiltredData(data);
+  };
+
+  const changeHandle = (e) => {
+    switch (e.target.value) {
+      case "all":
+        setFiltredData(data);
+        break;
+      case "completed":
+        setFiltredData(data.filter(item => item.completed === true));
+        break;
+      case "uncompleted":
+        setFiltredData(data.filter(item => item.completed === false));
+        break;
+      default:
+        break;
+    }
+  };
+
+  const todoItem = filtredData.length
+    ? filtredData.map((item) => {
+        return (
+          <TodoItem
+            title={item.title}
+            id={item.id}
+            isCompleted={item.completed}
+            key={item.id}
+            setUpdateId={setUpdateId}
+            inpRef={inpRef}
+            crre={item}
+            setControllerUpdateToAdd={setControllerUpdateToAdd}
+            setCurrentItem={setCurrentItem}
+          />
+        );
+      })
+    : "";
+  return (
+    <main>
+      <select className={classes.selectFilter} onChange={changeHandle}>
+        <option value="all">All</option>
+        <option value="completed">Completed</option>
+        <option value="uncompleted">Uncompleted</option>
+      </select>
+      <section className={classes.task_list}>{todoItem}</section>
+    </main>
+  );
 }
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,39 +1,26 @@
-import React, { useId, useState } from 'react';
-import TodoForm from '../../components/TodoForm/TodoForm';
+import React, { useEffect, useRef, useState } from "react";
+import TodoForm from "../../components/TodoForm/TodoForm";
 import Lists from "../../components/Lists/Lists";
+import Login from "../../components/Login/Login";
 
 export default function Layout() {
-  const [todoList, setTodoList] = useState([
-    {
-      id: useId(),
-      title: "lorem hello",
-      isCompleted: true
-    },
-    {
-      id:useId(),
-      title: "lorem hello",
-      isCompleted: false
-    },
-    {
-      id:useId(),
-      title: "lorem hello",
-      isCompleted: false
-    },
-    {
-      id:useId(),
-      title: "lorem hello",
-      isCompleted: false
-    },
-    {
-      id:useId(),
-      title: "lorem hello",
-      isCompleted: false
-    },
-  ]);
-  return (
+  const [token, setToken] = useState(null);
+  const [updateId, setUpdateId] = useState(0);
+  const inpRef = useRef(null);
+  const [controllerUpdateToAdd,setControllerUpdateToAdd] = useState(false);
+  const [ currnetItem, setCurrentItem] = useState({});
+
+  const todo = (
     <>
-        <TodoForm setTodoList={setTodoList}/>
-        <Lists todoList={todoList} />
+      <TodoForm setToken={setToken} currnetItem={currnetItem} controllerUpdateToAdd={controllerUpdateToAdd} inpRef={inpRef} setUpdateId={setUpdateId} />
+      <Lists inpRef={inpRef} setCurrentItem={setCurrentItem} setControllerUpdateToAdd={setControllerUpdateToAdd} updateId={updateId} setUpdateId={setUpdateId} />
     </>
-  )
+  );
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    setToken(token);
+  }, []);
+
+  return token ? todo : <Login setToken={setToken} />;
 }
